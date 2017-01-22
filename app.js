547f09554b8fcdf1719318281640481961137a6a
Christian R <Christian R>
2017-01-22 16:29:52 -0700

    Added an extra conditional that checks if the requested game has a price, fixing an exception thrown when a game doesn't appear to have a price for any reason

--- app.js
+++ app.js
@@ -8,17 +8,39 @@ var numTimesFailed = 0;
 
 var reduceNumTimesFailed = setInterval(function() {
 	if(numTimesFailed > 0) {
-		console.log('calming down..');
+		console.log('calming down.. numTimesFailed now ' + numTimesFailed);
 		numTimesFailed--;
 	}
 }, 15000);
 
 client.on('ready', function() {
-	console.log('Logged in as ' + client.user.username);
+	console.log('Sale bot logged in, updating applist');
+
+	// var httpParams = {
+	// 	host: 'api.steampowered.com',
+	// 	path: '/ISteamApps/GetAppList/v0001/'
+	// };
+
+	// http.request(httpParams, function(response) {
+	// 	var data = '';
+
+	// 	response.on('data', function (chunk) {
+	// 		data += chunk;
+	// 	});
+
+	// 	response.on('end', function () {
+	// 		try {
+	// 			fs.writeFileSync('applist.json', data);
+	// 		}
+	// 		catch (ex) {
+	// 			console.error(ex);
+	// 		}
+	// 	});
+	// }).end();
 });
 
 client.on('message', function(msg) {
-	var regexp = /^\!sale[^\w](.+)$/;
+	var regexp = /^\!sale[^\w](.+)$/i;
 	var match = msg.content.match(regexp);
 	var applist = obj.applist.apps.app;
 	var appid, gameTitle;
@@ -39,56 +61,54 @@ client.on('message', function(msg) {
 
 	if(appid == null) {
 		if(numTimesFailed < 4) {
-			msg.reply('Game not found. Try again.');
-		}
-		else if(numTimesFailed == 4) {
-			msg.reply('If you\'re going to keep making garbage requests, I won\'t help you.');
-		}
-		else if(numTimesFailed == 5) {
-			msg.reply('I\'m warning you, shitbird');
+			msg.reply('Game not found, try again');
 		}
 		else {
-			msg.reply('FUCC OFF');
+			msg.reply('If you\'re going to keep making garbage requests, I won\'t help you');
 		}
+
 		numTimesFailed++;
 		return;
 	}
+
+	console.log('Attempting to get sale info for app id ' + appid);
 
 	var httpParams = {
 		host: 'store.steampowered.com',
 		path: '/api/appdetails/?appids=' + appid
 	};
 
-	callback = function(response) {
+	http.request(httpParams, function(response) {
 		var str = '';
 
-		//another chunk of data has been recieved, so append it to `str`
 		response.on('data', function (chunk) {
 			str += chunk;
 		});
 
-		//the whole response has been recieved, so we just print it out here
 		response.on('end', function () {
 			var gameDetails = JSON.parse(str);
-			var initPrice = (gameDetails[appid].data.price_overview.initial / 100);
-			var origPrice = (gameDetails[appid].data.price_overview.final / 100);
-			var percentDiscount = gameDetails[appid].data.price_overview.discount_percent;
 
-			var formattedSalePrice = initPrice.toLocaleString("en-US", { style: "currency", currency: "USD" });
-			//var formattedOrigPrice = origPrice.toLocaleString("en-US", { style: "currency", currency: "USD" });
+			if(gameDetails[appid].data != null && gameDetails[appid].data.price_overview != null) {
+				var initPrice = (gameDetails[appid].data.price_overview.initial / 100);
+				var origPrice = (gameDetails[appid].data.price_overview.final / 100);
+				var percentDiscount = gameDetails[appid].data.price_overview.discount_percent;
 
-			if(percentDiscount > 0) {
-				msg.reply('\n\nCurrent sale price of ' + gameTitle + ': \*\*' + 
-					formattedSalePrice + '\*\*\nDiscount percent: \*\*-' + percentDiscount + '%\*\*' + 
-					'\nhttp://store.steampowered.com/app/' + appid);
+				var formattedSalePrice = initPrice.toLocaleString("en-US", { style: "currency", currency: "USD" });
+
+				if(percentDiscount > 0) {
+					msg.reply('\n\nCurrent sale price of ' + gameTitle + ': \*\*' + 
+						formattedSalePrice + '\*\*\nDiscount percent: \*\*-' + percentDiscount + '%\*\*' + 
+						'\nhttp://store.steampowered.com/app/' + appid);
+				}
+				else {
+					msg.reply('Sorry, \*' + gameTitle + '\* is not on sale right now');
+				}
 			}
 			else {
-				msg.reply('Sorry, \*' + gameTitle + '\* is not on sale right now');
+				msg.reply('Something went wrong with your request, maybe that game is free or doesn\'t exist?');
 			}
 		});
-	}
-
-	http.request(httpParams, callback).end();
+	}).end();
 });
 
 client.on('error', function(err) {
